79034a0845cbb3d0a45bc890679364bf82d77ee2
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-11-07 09:41:24 +0000

    Updated GYRE test data; added input files to generate

--- tests/test_gyre.py
+++ tests/test_gyre.py
@@ -10,17 +10,21 @@ class TestGYREFunctions(unittest.TestCase):
     def test_load_summary(self):
         header, data = gyre.load_summary('data/gyre.summ')
         self.assertAlmostEqual(header['M_star'], 1.989e33)
-        self.assertAlmostEqual(header['R_star'], 6.959894677e10)
+        self.assertAlmostEqual(header['R_star'], 6.959906258e10)
         for i, row in enumerate(data):
-            self.assertEqual(row['l'], 1000)
-            self.assertEqual(row['n_pg'], i)
+            self.assertEqual(row['l'], 1)
+            self.assertEqual(row['n_pg'], i+19)
 
     def test_load_mode(self):
         for i in range(3):
-            header, data = gyre.load_mode('data/gyre.mode%i' % (i+1))
-            self.assertEqual(header['n_pg'], i)
-            for k in ['x','Rexi_r','Imxi_r','Rexi_h','Imxi_h']:
-                self.assertAlmostEqual(data[k][0], 0.0)
+            header, data = gyre.load_mode('data/gyre.mode_%i' % (i+1))
+            self.assertEqual(header['n_pg'], i+19)
+            self.assertEqual(header['l'], 1)
+            self.assertEqual(header['Imomega'], 0.0)
+            self.assertEqual(header['Imfreq'], 0.0)
+            for row in data:
+                self.assertEqual(row['Imxi_r'], 0.0)
+                self.assertEqual(row['Imxi_h'], 0.0)
 
 
 if __name__ == '__main__':
