38d0d6debf608141750d2d2af68984bc66db4a5e
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-11-07 11:03:54 +0000

    This version successfully uploaded to PyPI test site.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'TOMSO',
+    packages = ['tomso'],
+    version = '0.0.2',
+    description = 'Tools for Modelling Stars and their Oscillations',
+    author = 'Warrick Ball',
+    author_email = 'wball@bison.ph.bham.ac.uk',
+    url = 'https://github.com/warrickball/tomso',
+    download_url = 'https://github.com/warrickball/tomso/archive/v0.0.2.tar.gz',
+    keywords = [],
+    classifiers = [],
+    licence = 'MIT'
+    )
