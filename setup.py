111f2e5ec1668a923c2f7d55a67d9ac244f63d79
Warrick Ball <wball@phy-hrs-171.ph.bham.ac.uk>
2017-11-07 11:31:42 +0000

    PyPI preparation and refinement

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'TOMSO',
@@ -9,6 +9,7 @@ setup(
     author_email = 'wball@bison.ph.bham.ac.uk',
     url = 'https://github.com/warrickball/tomso',
     download_url = 'https://github.com/warrickball/tomso/archive/v0.0.2.tar.gz',
+    install_requires=['numpy'],
     keywords = [],
     classifiers = [],
     licence = 'MIT'
