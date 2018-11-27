37b2b586d1783f52ee160b46601a29126563ded8
Warrick Ball <W.H.Ball@bham.ac.uk>
2018-11-27 10:32:50 +0000

    Updated MESA tests to match renamed load_astero_results and added (possibly redundant) tests of gzipped data

--- tests/test_mesa.py
+++ tests/test_mesa.py
@@ -18,8 +18,30 @@ class TestMESAFunctions(unittest.TestCase):
             self.assertLessEqual(history['star_age'][i],
                                  history['star_age'][i+1])
 
+    def test_gzipped_load_history(self):
+        header, history = mesa.load_history('data/mesa.history.gz')
+        self.assertEqual(header['version_number'], 10108)
+        self.assertAlmostEqual(header['initial_mass'], 0.9995)
+        self.assertAlmostEqual(header['initial_z'], 0.02)
+
+        for i, row in enumerate(history[:-1]):
+            self.assertLessEqual(history['model_number'][i],
+                                 history['model_number'][i+1])
+            self.assertLessEqual(history['star_age'][i],
+                                 history['star_age'][i+1])
+
     def test_load_profile(self):
         header, profile = mesa.load_profile('data/mesa.profile')
+        self.assertEqual(header['model_number'], 95)
+        self.assertEqual(header['num_zones'], 559)
+        self.assertAlmostEqual(header['initial_mass'], 0.9995)
+        self.assertAlmostEqual(header['initial_z'], 0.02)
+
+        for i in range(len(profile)):
+            self.assertEqual(profile['zone'][i], i+1)
+
+    def test_load_gzipped_profile(self):
+        header, profile = mesa.load_profile('data/mesa.profile.gz')
         self.assertEqual(header['model_number'], 95)
         self.assertEqual(header['num_zones'], 559)
         self.assertAlmostEqual(header['initial_mass'], 0.9995)
@@ -40,8 +62,26 @@ class TestMESAFunctions(unittest.TestCase):
             for sigma in sample['l%i' % ell]['err']:
                 self.assertAlmostEqual(sigma, 0.3)
 
-    def test_load_results_data(self):
-        results = mesa.load_results_data('data/simplex_results.data')
+    def test_load_gzipped_sample(self):
+        sample = mesa.load_sample('data/mesa.sample.gz')
+        self.assertAlmostEqual(sample['mass/Msun'], 0.9995)
+        self.assertAlmostEqual(sample['csound_rms'], 0.0)
+        self.assertAlmostEqual(sample['Teff_sigma'], 65.0)
+        self.assertAlmostEqual(sample['logL_sigma'], 0.05)
+        self.assertAlmostEqual(sample['logg_sigma'], 0.06)
+        self.assertAlmostEqual(sample['FeH_sigma'], 0.05)
+        for ell in range(4):
+            for sigma in sample['l%i' % ell]['err']:
+                self.assertAlmostEqual(sigma, 0.3)
+
+    def test_load_astero_results(self):
+        results = mesa.load_astero_results('data/simplex_results.data')
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results[0]['sample'], 1)
+        self.assertEqual(results[1]['sample'], 2)
+
+    def test_load_gzipped_astero_results(self):
+        results = mesa.load_astero_results('data/simplex_results.data.gz')
         self.assertEqual(len(results), 2)
         self.assertEqual(results[0]['sample'], 1)
         self.assertEqual(results[1]['sample'], 2)
