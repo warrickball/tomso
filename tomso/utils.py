04e2a4a81ff758726148f020ce90e70509c2107a
Warrick Ball <W.H.Ball@bham.ac.uk>
2020-08-15 22:19:28 +0100

    Suppress warnings about escaped characters in utils docstrings

--- tomso/utils.py
+++ tomso/utils.py
@@ -9,14 +9,14 @@ by all modules."""
 
 def integrate(y, x):
     """Integral of `y` over `x`, computed using the trapezoidal rule. 
-    i.e. :math:`\int _{x[0]} ^x y(x') dx'`."""
+    i.e. :math:`\\int _{x[0]} ^x y(x') dx'`."""
     dz = (y[1:]+y[:-1])/2.*np.diff(x)
     return np.hstack((0., np.cumsum(dz)))
 
 
 def complement(y, x):
     """Complement of integral of `y` over `x`, computed using the
-    trapezoidal rule.  i.e. :math:`\int _x^{x[-1]}y(x') dx'`."""
+    trapezoidal rule.  i.e. :math:`\\int _x^{x[-1]}y(x') dx'`."""
     z = integrate(y, x)
     return z[-1] - z
 
