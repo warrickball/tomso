d117d88bfb56c2a2c5c3b0139e3bcaac697ce99d
Warrick Ball <warrickball@gmail.com>
2022-03-31 16:51:30 +0100

    remove fgong.fgong_get in favour of FGONG object properties

--- tests/test_fgong.py
+++ tests/test_fgong.py
@@ -44,39 +44,23 @@ class TestFGONGFunctions(unittest.TestCase):
         np.testing.assert_allclose(m1.glob, m2.glob)
         np.testing.assert_allclose(m1.var, m2.var)
 
-    def test_fgong_get(self):
+    def test_fgong_properties(self):
         m = fgong.load_fgong('data/modelS.fgong')
         np.testing.assert_equal(m.x, m.r/m.R)
         self.assertAlmostEqual(m.M, 1.989e33)
         self.assertAlmostEqual(m.R, 6.959894677e10)
-        np.testing.assert_allclose(m.cs**2, m.cs2)
+        np.testing.assert_allclose(m.cs**2, m.cs2, rtol=4*EPS)
+        np.testing.assert_allclose(m.q, m.m/m.M, rtol=4*EPS)
+        np.testing.assert_allclose(m.x, m.r/m.R, rtol=4*EPS)
+        np.testing.assert_allclose(m.Hp[:-1], (m.P/m.rho/m.g)[:-1], rtol=4*EPS)
+        np.testing.assert_allclose(m.cs2, m.Gamma_1*m.P/m.rho, rtol=4*EPS)
 
-        self.assertRaises(ValueError, fgong.fgong_get, ['asdf'], m.glob, m.var)
-
-    def test_fgong_get_reverse(self):
+    def test_fgong_properties_reverse(self):
         m = fgong.load_fgong('data/modelS.fgong')
         m_rev = fgong.load_fgong('data/modelS.fgong')
         m_rev.var = m_rev.var[::-1]
         for attr in ['x', 'rho', 'P', 'tau', 'cs']:
             np.testing.assert_allclose(getattr(m, attr), getattr(m_rev, attr)[::-1])
-
-    def test_fgong_get_cross_check(self):
-        m = fgong.load_fgong('data/modelS.fgong')
-        M, R, L, r, x, m, q, g, rho, P, Hp, G1, T, X, L_r, kappa, epsilon, cs2, cs, tau \
-            = fgong.fgong_get(['M','R', 'L', 'r', 'x', 'm', 'q', 'g',
-                               'rho', 'P', 'Hp', 'G1', 'T', 'X', 'L_r',
-                               'kappa', 'epsilon', 'cs2', 'cs', 'tau'],
-                           m.glob, m.var)
-        np.testing.assert_allclose(q, m/M, rtol=4*EPS)
-        np.testing.assert_allclose(x, r/R, rtol=4*EPS)
-        np.testing.assert_allclose(Hp[:-1], (P/rho/g)[:-1], rtol=4*EPS)
-        np.testing.assert_allclose(cs2, G1*P/rho, rtol=4*EPS)
-
-        m = fgong.load_fgong('data/modelS.fgong')
-        np.testing.assert_allclose(m.q, m.m/m.M, rtol=4*EPS)
-        np.testing.assert_allclose(m.x, m.r/m.R, rtol=4*EPS)
-        np.testing.assert_allclose(m.Hp[:-1], (m.P/m.rho/m.g)[:-1], rtol=4*EPS)
-        np.testing.assert_allclose(m.cs2, m.Gamma_1*m.P/m.rho, rtol=4*EPS)
 
 if __name__ == '__main__':
     unittest.main()
